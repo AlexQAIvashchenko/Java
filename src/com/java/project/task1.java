2fd241eb8f401e7537feb45bfdbd142c36f721ae
Alex <aleksandraivashenko32@gmail.com>
2018-10-03 21:21:59 +0300

    task #1

--- src/com/java/project/task1.java
+++ src/com/java/project/task1.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class task1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter name  - Alex");
         String name = sc.nextLine();
         System.out.println("Welcome to Java world " + name);
     }
